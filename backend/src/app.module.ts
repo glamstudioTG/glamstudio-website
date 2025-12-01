6cd90bf6ddcdeefed4a33b8e1678bd4c820f85b9
esteban_castañeda <esteban2306dd@gmail.com>
2025-12-01 09:48:51 -0500

    create prisma module and service to use client prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,8 +4,9 @@ import { ServicesModule } from './services/services.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BookingModule, ServicesModule, ScheduleModule, UsersModule, AuthModule],
+  imports: [BookingModule, ServicesModule, ScheduleModule, UsersModule, AuthModule, PrismaModule],
 })
 export class AppModule { }
