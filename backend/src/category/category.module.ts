4225970b4b899ad53c7a29d1a07e79ac1fcd1b15
Luis Esteban Castaneda Luque <estebancastaneda@MacBook-Air-de-Luis.local>
2025-12-10 17:21:50 -0500

    fixed error async await use in category service and union whit modules

--- backend/src/category/category.module.ts
+++ backend/src/category/category.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CategoryService } from './category.service';
-
+import { CategoryController } from './category.controller';
 @Module({
   imports: [PrismaModule],
+  controllers: [CategoryController],
   providers: [CategoryService],
   exports: [CategoryService],
 })
--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -7,7 +7,31 @@ export class CategoryService {
   constructor(private prisma: PrismaService) {}
 
   async createCategory(dto: CreateCategoryDto) {
-    return this.prisma.category.create({
+    return await this.prisma.category.create({
+      data: dto,
+    });
+  }
+
+  async getAllCategories() {
+    return await this.prisma.category.findMany();
+  }
+
+  async getCategoryById(id: string) {
+    return await this.prisma.category.findUnique({
+      where: { id },
+      select: { id: true, name: true },
+    });
+  }
+
+  async deleteCategory(id: string) {
+    return await this.prisma.category.delete({
+      where: { id },
+    });
+  }
+
+  async updateCategory(id: string, dto: any) {
+    return await this.prisma.category.update({
+      where: { id },
       data: dto,
     });
   }
