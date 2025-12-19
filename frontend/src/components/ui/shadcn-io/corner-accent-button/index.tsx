a89db9a1897852e4c05ed826db1835ee5d11a64f
Luis Esteban Castaneda Luque <estebancastaneda@MacBook-Air-de-Luis.local>
2025-12-19 13:45:31 -0500

    create hero section of landing page

---
+++ frontend/src/components/ui/shadcn-io/corner-accent-button/index.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import { cn } from "@/lib/utils";
+import calendarIcon from "@/public/icons/calendarIcon.png";
+import Image from "next/image";
+
+export interface CornerAccentButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode;
+}
+
+export const CornerAccentButton = React.forwardRef<
+  HTMLButtonElement,
+  CornerAccentButtonProps
+>(({ className, children = "Get Started", ...props }, ref) => {
+  return (
+    <button
+      ref={ref}
+      className={cn(
+        "relative flex items-center px-6 py-3 overflow-hidden font-medium transition-all bg-[#E0C49C] rounded-md group",
+        className
+      )}
+      {...(props as any)}
+    >
+      <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-[#a46306] rounded group-hover:-mr-4 group-hover:-mt-4">
+        <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-[#FFEEE4]" />
+      </span>
+
+      <span className="absolute bottom-0 rotate-180 left-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-[#a46306] rounded group-hover:-ml-4 group-hover:-mb-4">
+        <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-[#FFEEE4]" />
+      </span>
+
+      <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full bg-[#AF804F] rounded-md group-hover:translate-x-0" />
+
+      <span className="relative w-full text-left text-black transition-colors duration-200 ease-in-out group-hover:text-black">
+        <Image
+          src={calendarIcon}
+          alt="Calendar Icon"
+          width={20}
+          height={20}
+          className="mr-2 mb-1 inline-block"
+        />
+        {children}
+      </span>
+    </button>
+  );
+});
+
+CornerAccentButton.displayName = "CornerAccentButton";
--- frontend/src/modules/landing/components/hero/HeroSection.tsx
+++ frontend/src/modules/landing/components/hero/HeroSection.tsx
@@ -1,12 +1,59 @@
-import heroImage from '@/public/landing/Group 21.png'
-import Image from 'next/image'
+import heroImage from "@/public/images/landing/heroImage.png";
+import { CornerAccentButton } from "@/src/components/ui/shadcn-io/corner-accent-button";
+import { MagneticButton } from "@/src/components/ui/shadcn-io/magnetic-button";
+import Image from "next/image";
 
 export default function HeroSection() {
-    return (
-        <>
-            <h1>Hero Section</h1>
-            <Image src={heroImage} alt="Hero Image" />
-            
-        </>
-    )
+  return (
+    <>
+      <section className="relative w-full min-h-[600px] bg-[#FFEEE4] overflow-hidden flex items-center">
+        <div className="absolute inset-0 flex justify-end">
+          <div className="relative w-1/2 h-full">
+            <Image
+              src={heroImage}
+              alt="Hero Image"
+              fill
+              className="object-cover"
+              priority
+            />
+          </div>
+        </div>
+
+        <div className="absolute inset-0 pointer-events-none">
+          <div
+            className="
+        absolute top-0 left-1/2
+        h-full w-80
+        -translate-x-1/2
+        bg-gradient-to-r from-[#ffeee4] 
+      "
+          />
+        </div>
+        <div className="relative z-10 max-w-7xl mx-auto w-full grid grid-cols-1 md:grid-cols-2 px-20 items-center">
+          <div className="flex flex-col gap-6 max-w-md">
+            <span className="text-gray-500 text-sm tracking-wide font-sans">
+              Estudio de belleza premium
+            </span>
+
+            <h1 className="text-4xl md:text-7xl text-black font-mono">
+              Redefinimos tu brillo con el arte de la belleza.
+            </h1>
+
+            <p className="text-black font-sans">
+              En GlamStudio cuidamos cada detalle para resaltar tu estilo con
+              elegancia y confianza
+            </p>
+
+            <div className="flex items-center gap-10 pt-4">
+              <CornerAccentButton>Reserva tu cita</CornerAccentButton>
+
+              <MagneticButton className="bg-amber-50">
+                Ver nuestros servicios
+              </MagneticButton>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
