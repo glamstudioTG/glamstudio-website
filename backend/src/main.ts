f14dc745b2b22a1a04bbe61bdf497d88914265b8
Luis Esteban Castaneda Luque <estebancastaneda@MacBook-Air-de-Luis.local>
2025-12-27 12:12:48 -0500

    merge landing_home_page into frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
