795f3d95ff74f84cefc8c1a3b443291451ffaf4b
Luis Esteban Castaneda Luque <estebancastaneda@MacBook-Air-de-Luis.local>
2026-03-19 18:15:19 -0500

    refactor: update useCreateBooking mutation to use CreateBookingPayload instead of BookingDraft

--- frontend/src/modules/booking/hooks/query/useCategoryBooking.ts
+++ frontend/src/modules/booking/hooks/query/useCategoryBooking.ts
@@ -1,9 +1,9 @@
 import { useMutation } from "@tanstack/react-query";
 import { BookingApi } from "@/src/modules/booking/service/api/booking.api";
-import { BookingDraft } from "@/src/modules/booking/types/booking.types";
+import { CreateBookingPayload } from "@/src/modules/booking/types/booking.types";
 
 export function useCreateBooking() {
   return useMutation({
-    mutationFn: (draft: BookingDraft) => BookingApi.create(draft),
+    mutationFn: (payload: CreateBookingPayload) => BookingApi.create(payload),
   });
 }
